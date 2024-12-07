d5d65edd77cf7a138a973dc8e07e0e13f7c3e2f5
Abyl239 <altajanovabylay@gmail.com>
2024-12-07 16:41:45 +0600

    feat/hashrouting

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -17,21 +17,21 @@ const HomePage: React.FC = () => {
           <p className={styles.featureDescription}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
-          <i className={"bi bi-house-door "+ styles['icon']}></i>
+          <i className={"bi bi-people "+ styles['icon']}></i>
           </div>
         <div className={styles.feature}>
           <h2 className={styles.featureTitle}>Feature 2</h2>
           <p className={styles.featureDescription}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
-          <i className={"bi bi-house-door "+ styles['icon']}></i>
+          <i className={"bi bi-rocket "+ styles['icon']}></i>
           </div>
         <div className={styles.feature}>
           <h2 className={styles.featureTitle}>Feature 3</h2>
           <p className={styles.featureDescription}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
-          <i className={"bi bi-house-door "+ styles['icon']}></i>
+          <i className={"bi bi-globe "+ styles['icon']}></i>
           </div>
       </section>
     </div>
